4015a41bbc118920e4a54964f00320a8d1b6e28b
renzkie1143 <renzkie1143@gmail.com>
2022-09-15 16:09:32 +0800

    update style

---
+++ To-Do-List/app.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',() =>{
+    const form = document.querySelector('#add-list');
+    const input = document.querySelector('#input-to-do-list');
+    const list_el = document.querySelector('#task');
+    
+})
