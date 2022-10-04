ec55a213f8d21a896ccd1effcf979d4e332c76b2
renzkie1143 <renzkie1143@gmail.com>
2022-10-04 10:34:24 +0800

    newUpdate-3.0

--- javaScriptDOM/app.js
+++ javaScriptDOM/app.js
@@ -3,24 +3,23 @@ let inputField = document.querySelector("#inpuField");
 let toDoContainer = document.querySelector("#toDoContainer");
 let checker = [];
 addBtn.addEventListener("click",() =>{
-
+    // creating elements in order to display the users input
     let toDoList = document.createElement("li");
     let output = document.createElement("output")
     output.innerHTML = inputField.value;
     output.classList.add("toDoList") 
-    // check the existing value if not exist it return -1
+    // passing the value of the users input 
     let toDoListValue = output.innerHTML;
+    // check the existing value if not exist it return -1
     let toDoListValueLowerCase = checker.findIndex(value => {
         return value.toLowerCase() === toDoListValue.toLowerCase();
     })
-    
-
     // create element for the removebuttom
     let removeBtn = document.createElement("button");
     removeBtn.innerHTML = "Delete";
     removeBtn.classList.add("removeBtn");
 
-    
+    // validate if the input is aleardy exist
     if (  toDoListValue == ""){
         alert(" Please fill out")
     }else if (checker[0] != undefined && toDoListValueLowerCase != -1){ 
@@ -31,9 +30,7 @@ addBtn.addEventListener("click",() =>{
         toDoList.append(output)
         toDoList.append(removeBtn);
     }
-
-    console.log(toDoListValueLowerCase)
-    
+    // delete the users input
     removeBtn.addEventListener("click", (e) =>{ 
         let deleteInput = e.target.parentNode;
         let innerValue = deleteInput.querySelector(".toDoList").innerHTML
@@ -42,6 +39,7 @@ addBtn.addEventListener("click",() =>{
                 let targetLock = checker.indexOf(value)
                 checker.splice(targetLock,1)
                 deleteInput.remove();
+                alert(innerValue + " has been deleted")
             } else{
                 return
             }
